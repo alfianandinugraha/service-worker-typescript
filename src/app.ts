56c413bcbce566a5a8f5e945e66cd6d5373c3704
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-12-29 12:12:58 +0700

    fix(sw): fix path to register service worker

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener("load", () => {
     navigator
       .serviceWorker
-      .register("./sw.js")
+      .register("/service-worker-typescript/sw.js")
       .then(() => console.log("registration success !"))
       .catch(() => console.error("registration failed !"))
   })
